3df29bbd4ad8bb26ca59ed49981a1887653fc6e1
Juanjo Torres <juanjotorres90@gmail.com>
2026-01-11 19:40:53 +0100

    fix(api): remove duplicate eslint configs causing plugin conflict

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,12 +1,6 @@
 import { config as baseConfig } from "@repo/eslint-config/base";
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
 
 export default [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
   ...baseConfig,
   {
     ignores: ["dist/**", "node_modules/**"],
